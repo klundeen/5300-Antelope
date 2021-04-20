527b59839dd4dad8d752c6f8e902cf641f42f26d
Harsha Thulasi <harsha0417@gmail.com>
2021-04-20 00:00:06 -0700

    removing comments

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -413,7 +413,6 @@ void HeapTable::close()
 Handle HeapTable::insert(const ValueDict *row)
 {
   this->open();
-  cout << "about to call append() in insert()" << endl;
   return this->append(this->validate(row));
 }
 
@@ -449,7 +448,6 @@ ValueDict *HeapTable::validate(const ValueDict *row)
 
   for (Identifier colName : this->column_names)
   {
-    // cout << "validate(): working on colName: " << colName << endl;
     ColumnAttribute colAttr = this->column_attributes[col_num++];
     ValueDict::const_iterator found = row->find(colName);
     Value value = found->second;
@@ -459,9 +457,7 @@ ValueDict *HeapTable::validate(const ValueDict *row)
     }
     else
     {
-      // cout << "validate: about to get value for colName: " << colName << endl;
       value = row->at(colName);
-      // cout << "validate: value is: " << value.data_type << endl;
       validatedRow->insert(pair<Identifier, Value>(colName, value));
     }
   }
@@ -491,24 +487,18 @@ Handle HeapTable::append(const ValueDict *row)
 
 Dbt *HeapTable::marshal(const ValueDict *row)
 {
-  char *bytes = new char[DbBlock::BLOCK_SZ]; // more than we need (we insist that one row fits into DbBlock::BLOCK_SZ)
+  char *bytes = new char[DbBlock::BLOCK_SZ];
   uint offset = 0;
   uint col_num = 0;
   for (auto const &column_name : this->column_names)
   {
-    cout << "marshal(): working on colName: " << column_name << endl;
-    cout << "marshal(): this->col_attr len: " << this->column_attributes.size() << endl;
     ColumnAttribute ca = this->column_attributes[col_num++];
-    cout << "mashal(): retreieved ca" << endl;
     ValueDict::const_iterator column = row->find(column_name);
-    cout << "mashal(): retreieved column from row: " << column->first << endl;
     Value value = column->second;
-    cout << "marshal():    col data type: " << ca.get_data_type() << endl;
     if (ca.get_data_type() == ColumnAttribute::DataType::INT)
     {
       *(int32_t *)(bytes + offset) = value.n;
       offset += sizeof(int32_t);
-      cout << "marshal():     done with column int offset" << endl;
     }
     else if (ca.get_data_type() == ColumnAttribute::DataType::TEXT)
     {
@@ -517,14 +507,12 @@ Dbt *HeapTable::marshal(const ValueDict *row)
       offset += sizeof(u_int16_t);
       memcpy(bytes + offset, value.s.c_str(), size); // assume ascii for now
       offset += size;
-      cout << "marshal():     done with column text offset" << endl;
     }
     else
     {
       throw DbRelationError("Only know how to marshal INT and TEXT");
     }
   }
-  cout << "marshal(): done with column/row attr match moving to creating data" << endl;
   char *right_size_bytes = new char[offset];
   memcpy(right_size_bytes, bytes, offset);
   delete[] bytes;
